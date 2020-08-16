47ebe2f09c25c0fd89497e9ca7651fdf0abe95cb
Santiago Cardona <santiagocardo80@gmail.com>
2020-08-16 16:17:05 -0500

    🔨 Change payment's button name to: place order

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -49,7 +49,7 @@ const ShoppingList = () => {
             </div>
             <div className="btn mt-4 text-white text-center border-primary lg:border-2 bg-primary hover:bg-red-400 transition ease-out duration-500">
               <svg fill="none" className="hidden md:inline-block w-8" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
-              <span className="ml-2 font-bold">Checkout Products</span>
+              <span className="ml-2 font-bold">place order</span>
             </div>
           </div>
         </div>
